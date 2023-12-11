bed3dc427b4bbf457b611ef7118b92e975fb7497
Felix.S <felix@nicebread.de>
2023-12-11 05:24:31 +0100

    Restructure app folder; many updates to profile

--- profile/profile_helpers.R
+++ profile/profile_helpers.R
@@ -1,4 +1,17 @@
-unCamel <- function(x) gsub("([A-Z]){1}", " \\1", x) |> str_trim()
+unCamel0 <- function(x) {
+  gsub("([A-Z]){1}", " \\1", x) |> str_trim()
+}
+
+unCamel <- function(df, cname) {
+  if (cname %in% colnames(df)) {
+    vec <-  df[, cname] |> unlist()
+    df[, cname] <- gsub("([A-Z]){1}", " \\1", vec) |> str_trim()
+  } else {
+    df[, cname] <- NA
+  }
+  
+  return(df)
+}
 
 # sc = scores object
 get_indicators <- function(sc, pattern) {
@@ -19,3 +32,16 @@ get_indicators <- function(sc, pattern) {
     rownames(res) <- NULL
     return(res[grepl(pattern, res$indicator), ])
 }
+
+
+theme_singlebar <- theme_minimal() + theme(
+  axis.text = element_blank(),       # Remove axis text
+  axis.title = element_blank(),      # Remove axis title
+  axis.ticks = element_blank(),      # Remove ticks
+  panel.grid.major = element_blank(),# Remove major grid
+  panel.grid.minor = element_blank(),# Remove minor grid
+  axis.line = element_blank(),        # Remove axis line
+  plot.title = element_text(face="bold", margin = margin(t = 0, b = -100, unit = "pt")),              # move plot title closer to bar
+  panel.spacing.x=unit(1, "lines"),   # add some extra space on the left side to make text labels visible
+  plot.margin = margin(t=0, b=-200, l=0, r=0, unit="pt")
+)
